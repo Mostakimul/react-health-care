bafb19abadc09772d9bb894e119164f41bf81e2b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-18 01:08:09 +0200

    router installed and basic router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="bg-gray-400">
-      <h2>Hello React</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
