8fea46dc0a6593548c1528987ce8560676afdf53
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-18 01:38:28 +0200

    centered container class globally

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h3>This is Home Page!</h3>
+      <div className="container">
+        <h3>This is Home Page!</h3>
+      </div>
     </div>
   );
 };
