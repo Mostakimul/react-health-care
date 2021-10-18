c05892b912cbb1290349bdd1c8ae485ce41f3d20
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-19 01:35:42 +0200

    Error Page completed

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 const Error = () => {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push('/home');
+  }
+
   return (
-    <div>
-      <h2>404 page not found!</h2>
+    <div className="h-screen text-center py-14 space-y-5">
+      <h2 className="text-6xl text-gray-800 font-bold">404!</h2>
+      <h3 className="text-4xl text-gray-800 font-semibold">
+        Sorry! The Page Not Found
+      </h3>
+      <p className="text-lg font-normal">
+        Oops! The page you are looking for does not exit. it might been moved or
+        deleted.
+      </p>
+      <button
+        onClick={handleClick}
+        className="bg-blue-800 text-white py-3 px-7 font-semibold rounded-md shadow"
+      >
+        Return to Home
+      </button>
     </div>
   );
 };
