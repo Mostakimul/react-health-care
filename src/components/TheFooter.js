4ac6831049a90f55cf62305320fd29d52d56c65c
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-18 23:47:39 +0200

    Footer added

---
+++ src/components/TheFooter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TheFooter = () => {
+  return (
+    <div className="bg-blue-900 py-2 text-center">
+      <p className="text-white font-semibold text-base">
+        Copyright &#169;2021 Developed by Mostakimul Karim
+      </p>
+    </div>
+  );
+};
+
+export default TheFooter;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TheCounter from '../components/TheCounter';
+import TheFooter from '../components/TheFooter';
 import TheHeader from '../components/TheHeader';
 
 const Home = () => {
@@ -8,9 +9,11 @@ const Home = () => {
       {/* The Header */}
       <TheHeader />
       {/* Counter Div */}
-      <div>
+      <div className="mb-4">
         <TheCounter />
       </div>
+      {/* Footer */}
+      <TheFooter />
     </div>
   );
 };
