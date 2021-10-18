1edd60ffb34d66a0332c3f1728dd45135a0cd601
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-19 00:00:43 +0200

    json data added and tested

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TheCounter from '../components/TheCounter';
 import TheFooter from '../components/TheFooter';
 import TheHeader from '../components/TheHeader';
 
 const Home = () => {
+  useEffect(() => {
+    fetch('/db.json')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div>
       {/* The Header */}
