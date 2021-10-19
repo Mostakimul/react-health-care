8d512eaa102138fdf035a5db12c48acc71b9da91
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-19 16:46:20 +0200

    hover changes and firebase initialized

--- src/components/Service.js
+++ src/components/Service.js
@@ -4,7 +4,7 @@ const Service = (props) => {
   const { title, details } = props.data;
   const TheIcon = props.data.icon;
   return (
-    <div className="bg-white rounded-lg shadow-lg text-center p-4">
+    <div className="bg-white rounded-lg hover:bg-gray-100 shadow-lg text-center p-4">
       <TheIcon className="w-20 h-20 mx-auto text-blue-700" />
       <h3 className="text-xl font-medium">{title}</h3>
       <p>{details}</p>
